29e4f75efb56406c435ed53dfd673f85acc1bd73
user <user@PC-MAIN-401>
2022-09-22 10:02:00 +0900

    ㅁ

---
+++ thisisjava/src/ch05/sec07/Array0921Exam3.java
@@ -0,0 +1,38 @@
+package ch05.sec07;
+
+public class Array0921Exam3 {
+
+	public static void main(String[] args) {
+		String[] names = {"홍길동","일지매","임꺽정","김길동"};
+		int[][] scores = {{80,83,85},{86,90,92},{88,87,95},{80,87,90}};
+		int[] total = new int[3];
+		int grandTotal=0;
+		
+		System.out.println("------------------성적표-----------------------");
+		System.out.print("|이름\t|국어\t|영어\t|수학\t|총점\t|평균");
+		System.out.println("\n----------------------------------------------");
+
+		
+		for(int i=0;i<scores.length;i++) {
+			int sum=0;
+			System.out.print("|"+names[i]+"\t|");
+			int j=0;
+			//향상된 for문으로 
+			for(int score:scores[i]) {
+				System.out.print(score+"\t|");
+				sum+=score;
+				total[j]+=score;
+				grandTotal+=score;
+			}
+		    System.out.print(sum+"\t|");
+		    System.out.printf("%5.2f\n",((double)sum/scores[i].length));
+		}
+		System.out.println("----------------------------------------------");
+		System.out.print("|총 합\t|");
+		for(int i=0;i<total.length;i++) {
+			System.out.print(total[i]+"\t|");
+		}
+		System.out.printf("%d \t|%5.2f\n",grandTotal,((double)grandTotal/(scores.length*scores[0].length)));
+		System.out.println("----------------------------------------------");
+	}
+}
