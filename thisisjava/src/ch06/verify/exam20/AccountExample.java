89251c74a21276b4554d3a2668b13fcc3c25d891
user <user@PC-MAIN-401>
2022-09-23 17:19:43 +0900

    a

--- thisisjava/src/ch06/verify/exam20/AccountExample.java
+++ thisisjava/src/ch06/verify/exam20/AccountExample.java
@@ -2,34 +2,35 @@ package ch06.verify.exam20;
 
 public class AccountExample {
 	public static void main(String[] args) {
-	 Account[] accounts = new Account[10];//[A][A][null][null][null][null][null][null][null][null]
-	 accounts[0] = new Account("111-111", "홍길동", 10000);
-	 accounts[1] = new Account("111-222", "일지매", 20000);
-	 
-	 for(int i=0;i<accounts.length;i++) {
-		 Account account =accounts[i];
-		 if(account!=null) {
-		 if(account.getAno().equals("111-111")) {
-			 account.setBalance((account.getBalance()-3000));
-			 System.out.println("출금 성공");
-		  }
-		 }
-	 }
+		Account[] accounts = new Account[10];// [A][A][null][null][null][null][null][null][null][null]
+		accounts[0] = new Account("111-111", "홍길동", 10000);
+		accounts[1] = new Account("111-222", "일지매", 20000);
+
+		for (int i = 0; i < accounts.length; i++) {
+			Account account = accounts[i];
+			if (account != null) {
+				if (account.getAno().equals("111-111")) {
+					account.setBalance((account.getBalance() - 3000));
+					System.out.println("출금 성공");
+				}
+			}
+		}
 	}
 }
 
-class Account{
+class Account {
 	private String ano;
 	private String owner;
 	private int balance;
+
 	public Account(String ano, String owner, int balance) {
 		this.ano = ano;
 		this.owner = owner;
 		this.balance = balance;
 	}
-	public int getBalance() {	return balance;	}
-	public void setBalance(int balance) {		this.balance = balance;}
-	public String getAno() {	return ano;	}
+
+	public int getBalance() { 		return balance;	}
+	public void setBalance(int balance) {		this.balance = balance;	}
+	public String getAno() {		return ano;	}
 	public String getOwner() {		return owner;	}
-	
 }
