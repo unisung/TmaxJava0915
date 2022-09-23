a4db97d1d3ad3775fc92afb3c51711eddf68f89a
user <user@PC-MAIN-401>
2022-09-23 09:54:36 +0900

    a

--- thisisjava/src/ch05/verify/Exam08.java
+++ thisisjava/src/ch05/verify/Exam08.java
@@ -10,10 +10,20 @@ public class Exam08 {
 		
 		int sum=0;
 		int count=0;
+		//향상된 for문
 		for(int[] a1:array) {	
 			for(int a:a1) {
 				sum+=a;
 				count++;
+			}
+		}
+		//int[] [95][86]
+		//int[] [83][92][96]
+		//int[] [78][83][93][87][88]
+		sum=0;
+		for(int i=0;i<array.length;i++) {
+			for(int j=0;j<array[i].length;j++) {
+				  sum+=array[i][j];
 			}
 		}
 		System.out.println("합: "+sum+", 평균: "+(double)sum/count);
