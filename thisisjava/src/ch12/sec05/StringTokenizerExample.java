b684c12a32b339c74e79080194d3d88d50562655
vctor <vctor@naver.com>
2022-09-29 17:43:41 +0900

    a

--- thisisjava/src/ch12/sec05/StringTokenizerExample.java
+++ thisisjava/src/ch12/sec05/StringTokenizerExample.java
@@ -5,11 +5,17 @@ import java.util.StringTokenizer;
 public class StringTokenizerExample {
 	public static void main(String[] args) {
 		String data1 = "홍길동&이수홍,박연수";
-		String[] arr = data1.split("&|,");//문자열을 특정기준으로 잘라서 배열로 리턴해주는 메소드
+		String[] arr = data1.split("&|,");//{"홍길동","이수홍","박연수"}
+		//문자열을 특정기준으로 잘라서 배열로 리턴해주는 메소드
 		for(String token:arr)
 			System.out.println(token);
+		System.out.println(arr[0]);
 		System.out.println("----------------");
 		
+		String data2 = "hong@www.naver.com";
+		String[] arr2=data2.split("@");
+		for(String a:arr2)
+			System.out.println(a);
 		//->
 		//->[홍길동]
 		//->[이수홍]
@@ -21,5 +27,4 @@ public class StringTokenizerExample {
 			System.out.println(st.nextToken());
 		}
 	}
-
 }
