6d5326daea19726e43cc83c8c7f1b358211e7f5b
vctor <vctor@naver.com>
2022-09-30 13:56:56 +0900

    a

---
+++ thisisjava/src/ch12/sec11/exam03/GeResourceExample.java
@@ -0,0 +1,17 @@
+package ch12.sec11.exam03;
+
+public class GeResourceExample {
+	public static void main(String[] args) {
+	  Class clazz = Car.class;
+	  
+	  //같은 경로 - 이미지명
+	  String photo1Path = clazz.getResource("photo1.jpg").getPath();
+	  //다른 경로 - 패스/이미지명
+	  String photo2Path = clazz.getResource("images/photo1.jpg").getPath();
+	  
+	  System.out.println(photo1Path);
+	  System.out.println(photo2Path);
+
+	}
+
+}
