85315f27aae27b1ebd349fb107738c42fffea863
vctor <vctor@naver.com>
2022-09-30 17:17:34 +0900

    a

--- thisisjava/src/ch12/verify/exam08/FileWriterExample2.java
+++ thisisjava/src/ch12/verify/exam08/FileWriterExample2.java
@@ -1,13 +1,18 @@
 package ch12.verify.exam08;
 
 import java.io.FileWriter;//문자단위 출력 스트림
+import java.util.Scanner;
 
 public class FileWriterExample2 {
 	public static void main(String[] args) {
-		try(FileWriter fw = new FileWriter("c:/temp/file.txt");) {
-			fw.write("Java");
+		try(FileWriter fw = new FileWriter("c:/temp/file.txt");
+			Scanner scanner = new Scanner(System.in);) {
+			System.out.println("메세지를 입력하세요");
+			String msg = scanner.nextLine();
+			fw.write(msg);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
+		System.out.println("종료");
 	}
 }
