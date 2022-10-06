29bcb7c122cc23ec533a2a8fb1d1aa4403c67d5e
vctor <vctor@naver.com>
2022-10-06 15:11:05 +0900

    a

---
+++ thisisjava/src/ch19/sec06/CreateJsonExample.java
@@ -0,0 +1,12 @@
+package ch19.sec06;
+
+import org.json.JSONObject;
+
+public class CreateJsonExample {
+	public static void main(String[] args) {
+		JSONObject root = new JSONObject();
+		
+		
+
+	}
+}
