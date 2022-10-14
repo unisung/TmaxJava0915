4dcdc533d98f0ee479c1880c087e1cf2e58f274c
vctor <vctor@naver.com>
2022-10-14 16:55:48 +0900

    a

--- oracle01/src/sql/ch13_objects.sql
+++ oracle01/src/sql/ch13_objects.sql
@@ -44,20 +44,131 @@ deptno number(2)
 
 select * from user_indexes;
 
+select * from user_tables;
+select * from dba_tables;
+select * from all_tables;
+
+/* view 정보 조회 */
+select * from user_views;
+
+/* view 생성 */
+create or replace view vw_emp20
+as 
+select empno, ename, job, deptno
+  from emp
+  where deptno = 20;
+
+select * from vw_emp20;
+
+/* 단순 view에 대이터 입력 */
+insert into vw_emp20 values(8888,'홍길동','MANAGER',20);
+  
+select * from emp;
+/* view 삭제 drop view 뷰명 */
+drop view vw_emp20;
 
 
+/* 읽기 전용 view 생성 */
+create or replace view vw_emp20
+as 
+select empno, ename, job, deptno
+  from emp
+  where deptno = 20
+with read only
+;
 
+select * from vw_emp20;
+/* ORA-42399: cannot perform a DML operation on a read-only view*/
+insert into vw_emp20 values(7777,'임꺽정','MANAGER',20);
 
+/* view 삭제 */
+drop view vw_emp20;
+/* view 생성 */
+create or replace view vw_emp20
+as 
+select empno, ename, job, deptno
+  from emp
+  where deptno = 20
+with check option
+;
+select * from vw_emp20;
+/* ORA-01402: view WITH CHECK OPTION where-clause violation */
+insert into vw_emp20 values(6666,'일지매','MANAGER',30);
 
+/* top-n */
+select rownum, e.*
+  from (select * from emp order by sal desc) e
+ where rownum <=3;
+ 
 
+ /* top-m~ n */
+select a.rn, a.empno, a.ename, a.sal
+ from 
+(select rownum as rn,emp.*
+  from emp
+ where rownum <=11
+ order by sal desc )a
+where rn >5  
+order by rn
+ ;
 
+ 
+/* 시퀀스 자동 발번 객체 sequence */
+create sequence seq_dept_sequence
+increment by 10
+start with 10
+maxvalue 90
+minvalue 0
+nocycle
+cache 2;
 
+create table dept_sequence 
+as 
+select * from dept where 1=0;
+select * from dept_sequence;
+/* sequence 조회 */
+select * from user_sequences;
+/* sequence를 이용한 데이타 입력 nextval, currval */
+/* sequnece의 옵션이 nocycle이므로 더이상 입력불가 */
+/* ORA-08004: sequence SEQ_DEPT_SEQUENCE.NEXTVAL exceeds MAXVALUE and cannot be instantiated*/
+insert into dept_sequence(deptno,dname,loc)
+ values (seq_dept_sequence.nextval,'DATABASE','SEOUL')
 
+select *from dept_sequence;
+ 
+/* cycle옵션 */ 
+create sequence seq_dept2_sequence
+increment by 10
+start with 10
+maxvalue 90
+minvalue 0
+cycle
+cache 2;
 
+create table dept2_sequence 
+as 
+select * from dept where 1=0;
 
+insert into dept2_sequence(deptno,dname,loc)
+ values (seq_dept2_sequence.nextval,'DATABASE','SEOUL')
 
+select * from dept2_sequence;
 
+/* 시퀀스 수정 */
+alter sequence seq_dept_sequence
+increment by 3
+maxvalue 99
+cycle;
 
+/* 시퀀스 삭제 drop sequence 시퀀스명 */
+drop sequence seq_dept_sequence
+
+/* 동의어 synonym */
+create public synonym e for emp;
+select * from e;
+
+/* 동의어 삭제 drop synonym 시노님명*/
+drop synonym e;
 
 
 
