94e8aa3132106e7bdc8a2a22295ad86a8a67b7c3
vctor <vctor@naver.com>
2022-10-14 13:17:26 +0900

    a

--- oracle01/src/sql/ch10_01_insert.sql
+++ oracle01/src/sql/ch10_01_insert.sql
@@ -205,14 +205,45 @@ update dept_temp2
                       from dept 
                      where deptno=40)              
  where deptno = 40;
+
+/* update 값 직접 입력, select로 수정*/ 
+update dept_temp2
+   set loc ='SEOUL',
+       dname=(select dname from dept where deptno=40)
+ where deptno=40;
  
+/* where조건에 sub쿼리 사용 */ 
+update dept_temp2
+   set loc ='PANGYO'
+ where deptno = (select deptno 
+                   from dept_temp2 
+                  where dname='RESEARCH');
+
+select * from dept_temp2;
+
+/* 데이타 삭제 delete from 테이블 where 조건 */
+select * from emp_temp;
+
+delete from emp_temp where job ='MANAGER';
+
+/* 삭제 조건에 sub쿼리 사용 */
+/* 급여등급 3 등급이고, 부서번호 30인 사원 정보 삭제 */
+select * from emp_temp2
+
+delete from emp_temp2
+where empno in 
+			(select empno 
+			  from emp, salgrade
+			 where emp.sal between losal and hisal
+			   and emp.deptno=30
+			   and salgrade.grade=3);
+			   
+/* 전체 데이터 삭제 */			   
+delete from emp_temp;
+select * from emp_temp;
  
-
-
-
-
-
- 
- 
-
+/*Data Manipulation Language */ 
+/** select(retrieve), insert(create), update, delete
+ *  CRUD
+ */
 
