17066c15e4c12d400e8d65fbeb2cbe3ee71899cf
vctor <vctor@naver.com>
2022-10-17 15:30:34 +0900

    ㅁ

--- oracle01/src/sql/ch15_privileges.sql
+++ oracle01/src/sql/ch15_privileges.sql
@@ -40,7 +40,35 @@ grant select,insert,delete on temp01 to hr;
 /* 한번에 여러권한 회수 */
 revoke insert, delete on temp01 from hr;
 
+/* Role 
+ * connect - alter session, create cluster, cerate database link,
+ * create sequnece, create session, create synonym, create table,
+ * create view
+ * 
+ * resource - creater trigger, create sequence, create type,
+ * create procedure, create cluster, create operator,
+ * create indextype, create table
+ * 
+ * dba - dba 권한 
+ * */
+/* 사용자 정의 role 생성하기 */
+SQL> connect sys as sysdba
+Enter password:
+Connected.
+SQL> create role rolestudy;/* 생성한 rolestudy */
 
+Role created.
+/* rolestudy에 개별 권한 담기 */
+SQL> grant connect, resource, create  view,
+  2  create synonym to rolestudy;
+
+Grant succeeded.
+/* role부여하기 */
+SQL> grant rolestudy to hr;
+
+Grant succeeded.
+
+SQL>
 
 
 
