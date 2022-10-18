022815497e93aef02353baf42541d9ff57bdcd57
vctor <vctor@naver.com>
2022-10-18 09:42:09 +0900

    10-18.001.

--- oracle01/src/sql/ch16_PL_SQL문.sql
+++ oracle01/src/sql/ch16_PL_SQL문.sql
@@ -386,6 +386,16 @@ end loop;
 end;
 /
 
+/*--------------  이중 반복문 구구단 ----------------*/
+begin
+for i in 2..9 loop
+  for j in 1..9 loop
+    dbms_output.put(i||'x'||j||'='||(i*j)||'   ');
+  end loop;
+   dbms_output.put_line('');
+end loop;
+end;
+/
 
 
 
