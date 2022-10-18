25e3525c940cbdd3e2dfa86248e4bf4ff0f2a1e9
vctor <vctor@naver.com>
2022-10-18 11:20:31 +0900

    a

--- oracle01/src/sql/ch17_record.sql
+++ oracle01/src/sql/ch17_record.sql
@@ -52,6 +52,7 @@ declare
    dname dept.dname%type,
    loc dept.loc%type
   );
+  
   dept_rec REC_DEPT;
 begin
 	dept_rec.deptno :=50;
@@ -64,8 +65,122 @@ begin
 end;
 /
   
+select * from dept_record;
 
-select * from dept_record;  
+
+/* 조회 */
+declare
+ type REC_DEPT is record(
+   deptno dept.deptno%type,
+   dname  dept.dname%type,
+   loc    dept.loc%type
+ );
+ type REC_EMP is record(
+   empno emp.empno%type,
+   ename emp.ename%type,
+   dinfo  REC_DEPT /* 중첩레코드 */
+ );
+ emp_rec REC_EMP;
+begin
+	select e.empno, e.ename, d.deptno, d.dname, d.loc
+	  into emp_rec.empno,emp_rec.ename,
+	       emp_rec.dinfo.deptno,
+	       emp_rec.dinfo.dname,
+	       emp_rec.dinfo.loc
+	  from emp e, dept d
+	 where e.deptno = d.deptno
+	   and e.empno = 7788;
+	   
+	dbms_output.put_line('EMPNO :'||emp_rec.empno);   
+	dbms_output.put_line('ENAME :'||emp_rec.ename);   
+	dbms_output.put_line('DEPTNO :'||emp_rec.dinfo.deptno);   
+	dbms_output.put_line('DNAME :'||emp_rec.dinfo.dname);   
+	dbms_output.put_line('LOC :'||emp_rec.dinfo.loc);   
+end;
+/
+ 
+/*  컬렉션 */
+declare
+ type ITAB_EX is table of varchar2(20) 
+ index by PLS_INTEGER;
+ 
+ text_arr ITAB_EX;
+ 
+begin
+	text_arr(1) :='1st data';
+	text_arr(2) :='2nd data';
+	text_arr(3) :='3rd data';
+	text_arr(4) :='4th data';
+	
+	dbms_output.put_line('text_arr(1) : '||text_arr(1));
+	dbms_output.put_line('text_arr(2) : '||text_arr(2));
+	dbms_output.put_line('text_arr(3) : '||text_arr(3));
+	dbms_output.put_line('text_arr(4) : '||text_arr(4));
+end;
+/
+
+/************* 레코드와 컬렉션 **************************/
+declare
+  type REC_DEPT is record(
+  deptno dept.deptno%type,
+  dname dept.dname%type
+  );
+  type ITAB_EX is table of REC_DEPT  
+  index by PLS_INTEGER; -- binary_integer
+  dept_arr ITAB_EX;
+  idx PLS_INTEGER :=0;
+BEGIN
+	for i in (select deptno, dname from dept) loop
+	   idx := idx+1;
+	   dept_arr(idx).deptno :=i.deptno;
+	   dept_arr(idx).dname :=i.dname;
+	   dbms_output.put_line(dept_arr(idx).deptno||
+	     ' : '||dept_arr(idx).dname);
+	end loop;
+END;
+/
+/* ---------- 컬렉션 함수들--------------------------- */
+declare
+type ITAB_EX is table of varchar2(20)
+ index by PLS_INTEGER;
+ 
+ text_arr ITAB_EX;
+begin
+	text_arr(1) :='1st data';
+	text_arr(2) :='2nd data';
+	text_arr(3) :='3rd data';
+	text_arr(50) :='50th data';
+	
+ dbms_output.put_line('text_arr.COUNT: '||text_arr.COUNT);
+ dbms_output.put_line('text_arr.FIRST: '||text_arr.FIRST);
+ dbms_output.put_line('text_arr.LAST: '||text_arr.LAST);
+ dbms_output.put_line('text_arr.PRIOR(50): '||text_arr.PRIOR(50));
+ dbms_output.put_line('text_arr.NEXT(50): '||text_arr.NEXT(50));
+		
+end;
+/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   
   
