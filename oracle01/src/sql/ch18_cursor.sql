ff4b3273fe4fb245bf3dcf2d3fd231cb62640e1d
vctor <vctor@naver.com>
2022-10-18 13:12:46 +0900

    ㅁ

--- oracle01/src/sql/ch18_cursor.sql
+++ oracle01/src/sql/ch18_cursor.sql
@@ -79,6 +79,77 @@ end;
 
 /
 
+/* 파라미터를 사용하는 커서 */
+/* cursor 커서명(파라미터 자료형,...) IS  select문... */
+declare
+   --명시적 커서 선언(declaration)
+	cursor c1 (p_deptno dept.deptno%type) is
+	  select deptno,dname,loc
+	    from dept
+	   where  deptno = p_deptno;
+  --커서 데이타를 담을 변수 선언
+    v_dept_row dept%rowtype;
+begin
+	 -- open 커서
+	 open c1(10);
+	 -- fetch 커서
+	 loop
+	   fetch c1 into v_dept_row;
+	   exit when c1%notfound;
+	   dbms_output.put_line('10 번부서-deptno'
+	           ||v_dept_row.deptno
+	           ||'dname-'||v_dept_row.dname
+	           ||'loc-'||v_dept_row.loc);
+	 end loop;
+	 --close 커서
+	 close c1;
+end;
+/
+
+/**-----------------------------------------*/
+declare
+   --명시적 커서 선언(declaration)
+	cursor c1 (p_deptno dept.deptno%type) is
+	  select deptno,dname,loc
+	    from dept
+	   where  deptno = p_deptno;
+  --커서 데이타를 담을 변수 선언
+    v_dept_row dept%rowtype;
+begin
+	 -- open 커서
+	 open c1(10);
+	 -- fetch 커서
+	 loop
+	   fetch c1 into v_dept_row;
+	   exit when c1%notfound;
+	   dbms_output.put_line('10 번부서-deptno'
+	           ||v_dept_row.deptno
+	           ||'dname-'||v_dept_row.dname
+	           ||'loc-'||v_dept_row.loc);
+	 end loop;
+	 --close 커서
+	 close c1;
+	 -- open 커서
+	 open c1(20);
+	 -- fetch 커서
+	 loop
+	   fetch c1 into v_dept_row;
+	   exit when c1%notfound;
+	   dbms_output.put_line('20 번부서-deptno'
+	           ||v_dept_row.deptno
+	           ||'dname-'||v_dept_row.dname
+	           ||'loc-'||v_dept_row.loc);
+	 end loop;
+	 --close 커서
+	 close c1;
+end;
+/
+
+    
+    
+	   
+	   
+	   
 
 
 
