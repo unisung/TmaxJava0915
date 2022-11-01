88391673ef647c5011ebdfd0033156d685e86c86
vctor <vctor@naver.com>
2022-11-01 13:24:45 +0900

    ㅁ

---
+++ thisisjava/src/ch20/UserInsertExample.java
@@ -0,0 +1,49 @@
+package ch20;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+//1. 드라이버 로딩
+//2. connection연결객체 얻기
+//3. sql문작성 dbms에 전달
+//4. 결과 확인
+//5. 자원해제
+public class UserInsertExample {
+	public static void main(String[] args) {
+	 //1. 
+		Connection conn=null;
+	 try {
+		  //1-1.드라이버로딩
+		 Class.forName("oracle.jdbc.driver.OracleDriver");
+		  //2.연결객체 인스턴스 생성
+		  String url="jdbc:oracle:thin:@localhost:1521:xe";
+		  String user="java";
+		  String password="oracle";
+		  conn = //factory패턴(GOF)
+		 DriverManager.getConnection(url, user, password);
+		//3. sql문작성 dbms에 전달
+		  String sql=new StringBuilder()
+				    .append("insert into users ")
+				    .append("(userid,username,userpassword,userage,useremail)")
+				    .append(" values('hong','홍길동','1234',18,'hong@naver.com')").toString();
+		  System.out.println(sql);
+		//3-2. 쿼리문 전달객체 생성
+		  Statement stmt = conn.createStatement();
+		//3-3. 쿼리문 전달 및 실행 
+		//-- insert/update/delete 일때 executeUpdate()
+		//return값은 입력/수정/삭제한 행의 수
+		  int result =stmt.executeUpdate(sql);
+		  if(result>0) System.out.println(result+"행이 입력됨");
+		  //java프로그램은 auto commit모드
+	 }catch(Exception e) {
+		 System.out.println(e.getMessage());
+	 }finally {
+		 try {
+			 conn.close();
+		 }catch (Exception e) {
+            System.out.println(e.getMessage());
+		}
+	 }
+	}
+}
