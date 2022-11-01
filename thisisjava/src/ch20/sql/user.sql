83e66bc766baa2d6665dbd7f63bd2b8823f24418
vctor <vctor@naver.com>
2022-11-01 15:09:53 +0900

    a

--- thisisjava/src/ch20/sql/user.sql
+++ thisisjava/src/ch20/sql/user.sql
@@ -6,3 +6,5 @@ select * from users where userage=25;
 select * from users where username like '%길동%';
 
 update users set userpassword ='1234' where userid='hong';
+
+delete from users where id='hong';
