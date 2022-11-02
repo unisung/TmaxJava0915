52ff36a76154a0192458e86cb1c732d804f70ff5
vctor <vctor@naver.com>
2022-11-02 09:28:32 +0900

    ㅁ

--- thisisjava/src/ch20/boards/Board.java
+++ thisisjava/src/ch20/boards/Board.java
@@ -1,8 +1,12 @@
 package ch20.boards;
 
+//참고 https://mindols.tistory.com/245
 import java.sql.Blob;
 import java.util.Date;
 
+import lombok.Data;
+
+@Data
 public class Board {
 	private int bno;
 	private String btitle;
@@ -11,84 +15,5 @@ public class Board {
 	private Date bdate;
 	private String bfilename;
     private Blob bfiledata;
-    //생성자
-	public Board() {}
-	public Board(int bno, String btitle, String bcontent, String bwriter, Date bdate, String bfilename,
-			Blob bfiledata) {
-		this.bno = bno;
-		this.btitle = btitle;
-		this.bcontent = bcontent;
-		this.bwriter = bwriter;
-		this.bdate = bdate;
-		this.bfilename = bfilename;
-		this.bfiledata = bfiledata;
-	}
-   //getters/setters
-	public int getBno() {
-		return bno;
-	}
-
-	public void setBno(int bno) {
-		this.bno = bno;
-	}
-
-	public String getBtitle() {
-		return btitle;
-	}
-
-	public void setBtitle(String btitle) {
-		this.btitle = btitle;
-	}
-
-	public String getBcontent() {
-		return bcontent;
-	}
-
-	public void setBcontent(String bcontent) {
-		this.bcontent = bcontent;
-	}
-
-	public String getBwriter() {
-		return bwriter;
-	}
-
-	public void setBwriter(String bwriter) {
-		this.bwriter = bwriter;
-	}
-
-	public Date getBdate() {
-		return bdate;
-	}
-
-	public void setBdate(Date bdate) {
-		this.bdate = bdate;
-	}
-
-	public String getBfilename() {
-		return bfilename;
-	}
-
-	public void setBfilename(String bfilename) {
-		this.bfilename = bfilename;
-	}
-
-	public Blob getBfiledata() {
-		return bfiledata;
-	}
-
-	public void setBfiledata(Blob bfiledata) {
-		this.bfiledata = bfiledata;
-	}
 	
-//toString()
-	@Override
-	public String toString() {
-		return "Board [bno=" + bno + ", btitle=" + btitle + ", bcontent=" + bcontent + ", bwriter=" + bwriter
-				+ ", bdate=" + bdate + ", bfilename=" + bfilename + ", bfiledata=" + bfiledata + "]";
-	}
-	
-	
-	
-    
-    
 }
