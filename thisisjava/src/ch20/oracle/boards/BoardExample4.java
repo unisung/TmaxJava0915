75a5f7e671774b12b9a488c91aeb5850c4f2cb10
vctor <vctor@naver.com>
2022-11-02 14:15:27 +0900

    a

--- thisisjava/src/ch20/oracle/boards/BoardExample4.java
+++ thisisjava/src/ch20/oracle/boards/BoardExample4.java
@@ -10,6 +10,7 @@ import ch20.boards.Board;
 
 public class BoardExample4 {
 	Scanner scanner = new Scanner(System.in);
+	Connection conn=null;
 	public static void main(String[] args) {
 	  BoardExample4 boardExample = new BoardExample4();
 	  boardExample.list();
@@ -22,7 +23,6 @@ System.out.println("----------------------------------------------------------")
 System.out.printf("%-6s%-12s%-16s%-40s\n", "no", "writer", "date", "title");
 System.out.println("----------------------------------------------------------");
 //
-Connection conn=null;
 try {
      conn=getConnection();
 	//3. sql문작성 dbms에 전달
@@ -83,11 +83,35 @@ public void clear() {System.out.println("***clear()메소드 실행 "); list();}
 public void read() {System.out.println("***read()메소드 실행 ");  list();}
 public void create() {
 	System.out.println("***create()메소드 실행 "); 
+//입력받기
+Board board = new Board();
+System.out.println("[새 게시물 입력]");
+System.out.print("제목: ");
+board.setBtitle(scanner.nextLine());
+System.out.print("내용: ");
+board.setBcontent(scanner.nextLine());
+System.out.print("작성자: ");
+board.setBwriter(scanner.nextLine());
 
-
+try {
+	 conn=getConnection();
+	 String sql="insert into boards(bno,btitle,bcontent,bwriter)"
+	 		  + " values(seq_bno.nextval,?,?,?)";
+		//3-2. 쿼리문 전달객체 생성
+	PreparedStatement pstmt = conn.prepareStatement(sql);
+	pstmt.setString(1,board.getBtitle());
+	pstmt.setString(2,board.getBcontent());
+	pstmt.setString(3,board.getBwriter());
+	
+	//3-3. 쿼리문 전달 및 실행 
+	pstmt.executeUpdate();	
+}catch(Exception e) {
+	e.printStackTrace();
+	exit();
+ }
 
 list();
-}
+}//create()메소드 끝.
 
 Connection getConnection() {
 	Connection conn = null;
