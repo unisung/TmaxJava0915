4d52b3087199605120b602b8c02dfac2d878c196
vctor <vctor@naver.com>
2022-11-07 17:11:20 +0900

    a

---
+++ student_study/src/main/java/service/StudentService.java
@@ -0,0 +1,23 @@
+package service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import model.Student;
+
+public class StudentService {
+	Map<Integer, Student> map=new HashMap<>();
+	
+StudentService(){
+map.put(1, new Student(1,"김길동","AA대학교","1999-10-21","kim@aa.com"));
+map.put(2, new Student(2,"박사랑","BB대학교","2000-01-21","park@bb.com"));
+map.put(3, new Student(3,"나최고","CC대학교","1998-07-11","na@cc.com"));
+map.put(4, new Student(4,"김길동","BB대학교","1999-03-10","kim@bb.com"));
+map.put(5, new Student(5,"홍길동","AA대학교","1999-12-10","hong@aa.com"));
+		
+		
+		
+		
+	}
+
+}
