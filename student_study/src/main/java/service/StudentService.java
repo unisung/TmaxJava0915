0674106f1259c1db71a47807555bc0b7dad70268
vctor <vctor@naver.com>
2022-11-07 17:13:14 +0900

    a

--- student_study/src/main/java/service/StudentService.java
+++ student_study/src/main/java/service/StudentService.java
@@ -1,6 +1,8 @@
 package service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import model.Student;
@@ -14,10 +16,13 @@ map.put(2, new Student(2,"박사랑","BB대학교","2000-01-21","park@bb.com"));
 map.put(3, new Student(3,"나최고","CC대학교","1998-07-11","na@cc.com"));
 map.put(4, new Student(4,"김길동","BB대학교","1999-03-10","kim@bb.com"));
 map.put(5, new Student(5,"홍길동","AA대학교","1999-12-10","hong@aa.com"));
-		
-		
-		
-		
 	}
 
+public List<Student> findAll(){
+	return new ArrayList<>(map.values());
+}
+
+public Student find(int seq) {
+	return map.get(seq);
+ }
 }
