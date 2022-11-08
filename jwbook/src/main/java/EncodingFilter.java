685ecacc7675327980669fc15bc33eb18c3f1c5a
vctor <vctor@naver.com>
2022-11-08 17:12:22 +0900

    a

--- jwbook/src/main/java/EncodingFilter.java
+++ jwbook/src/main/java/EncodingFilter.java
@@ -12,9 +12,6 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 
-/**
- * Servlet Filter implementation class EncodingFilter
- */
 @WebFilter(dispatcherTypes = {
 				DispatcherType.REQUEST, 
 				DispatcherType.FORWARD, 
@@ -23,25 +20,12 @@ import javax.servlet.http.HttpServletRequest;
 		}
 					, urlPatterns = { "/ListenerTestServlet" })
 public class EncodingFilter extends HttpFilter implements Filter {
-       
-    /**
-     * @see HttpFilter#HttpFilter()
-     */
     public EncodingFilter() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
+	public void destroy() {}
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
 	public void doFilter(ServletRequest request, 
 			             ServletResponse response, 
 			             FilterChain chain) throws IOException, ServletException {
@@ -53,8 +37,5 @@ public class EncodingFilter extends HttpFilter implements Filter {
 		chain.doFilter(request, response);
 	}
 
-	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
-	}
-
+	public void init(FilterConfig fConfig) throws ServletException {}
 }
