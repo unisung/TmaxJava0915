e8f0ff8b3535e7795e140ef4f34dae30087fdf3c
vctor <vctor@naver.com>
2022-11-09 14:33:07 +0900

    a

---
+++ jwbook/src/main/java/ch12/RestConfig.java
@@ -0,0 +1,17 @@
+package ch12;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestConfig extends Application {
+	public Map<String, Object> getProperties(){
+		Map<String, Object> properties = new HashMap<>();
+		properties.put("jersey.config.server.provider.packages", 
+				       "ch12");
+		return properties;
+	}
+}
