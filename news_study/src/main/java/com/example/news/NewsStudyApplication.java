b105ee093a6e9127d945c85e4f7a5b7dcbc85486
vctor <vctor@naver.com>
2022-11-10 15:06:44 +0900

    a

---
+++ news_study/src/main/java/com/example/news/NewsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsStudyApplication.class, args);
+	}
+
+}
