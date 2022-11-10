9d99dadb6b92e1ca5a90e4803ba5a1c090531768
vctor <vctor@naver.com>
2022-11-10 09:18:53 +0900

    a

--- spring_study/src/main/java/com/example/demo/TestWebController.java
+++ spring_study/src/main/java/com/example/demo/TestWebController.java
@@ -5,17 +5,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+//@Controller
 @RequestMapping("/test")
 public class TestWebController {
  
 	@GetMapping("/hello")
-	public String hello() {
+	public String hello(@RequestParam(value = "msg", required = false) String msg, Model m) {
+		// String msg = request.getParameter("msg");
+		m.addAttribute("msg",msg);
 		return "hello";
 		/* "/WBE-INF/views/" + "hello" +".jsp"
 		 *  -> /WBE-INF/views/hello.jsp"
 		 *  */
+	}
+	
+	@GetMapping("/hello/{msg}")
+	public String hello2(@PathVariable String msg) {
+		return msg;
 	}
 	
 	@GetMapping("/hello3/{msg}")
